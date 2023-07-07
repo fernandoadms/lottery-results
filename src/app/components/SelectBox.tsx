0f7f1a262153ed9ad50531d86ef0e16c67689110
Fernando Alves <fernando.alves0189@gmail.com>
2023-07-07 13:44:34 -0300

    feat(ui): Create the skeleton

---
+++ src/app/components/SelectBox.tsx
@@ -0,0 +1,12 @@
+export default function SelectBox() {
+  return (
+    <select name="lottery-select" id="lotterySelect">
+      <option value="mega-sena">Mega-sena</option>
+      <option value="quina">Quina</option>
+      <option value="lotofacil">Lotofácil</option>
+      <option value="lotomania">Lotomania</option>
+      <option value="timemania">Timemania</option>
+      <option value="diaDeSorte">Dia de sorte</option>
+    </select>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
+import SelectBox from "./components/SelectBox";
+
 export default function Home() {
-  return <h1>Lottery Results</h1>;
+  return (
+    <main>
+      <SelectBox />
+      <h1>Mega-Sena</h1>
+
+      <ul>
+        <li>06</li>
+        <li>09</li>
+        <li>28</li>
+        <li>33</li>
+        <li>37</li>
+        <li>40</li>
+      </ul>
+
+      <h2>Concurso</h2>
+
+      <h3>4531 – 07/04/2020</h3>
+
+      <p>
+        Este sorteio é meramente ilustrativo e não possui nenhuma ligação com a
+        CAIXA.
+      </p>
+    </main>
+  );
 }
